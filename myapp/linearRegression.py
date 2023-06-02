62b59866d8837eeb60e1e0b6ada8f924bbe0f7b2
Kester_Yang <31869286+sispuella@users.noreply.github.com>
2023-06-01 22:21:30 -0700

    Update linearRegression.py
    changed according to Erik's suggestions

--- myapp/linearRegression.py
+++ myapp/linearRegression.py
@@ -1,25 +1,32 @@
 import pandas as pd
-
-file_path = '../data/Course_Summary_2022_2023.json'
-df = pd.read_json(file_path)
-print(df.head())
+from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 
-X = df['Cap'].values.reshape(-1, 1)
-y = df['Enrolled'].values
+def perform_linear_regression(file_path):
+    df = pd.read_json(file_path)
 
-# Split the data into training and validation sets (70% validation, 30% training)
-X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=0.7, random_state=42)
+    # Extract features and target variables
+    X = df['Cap'].values.reshape(-1, 1)
+    y = df['Enrolled'].values
 
-from sklearn.linear_model import LinearRegression
+    # Split the data into training and validation sets
+    X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=0.7, random_state=42)
 
+    # Create a linear regression model
+    model = LinearRegression()
 
-model = LinearRegression()
+    # Fit the model to the training data
+    model.fit(X_train, y_train)
 
+    # Calculate R-squared score on the validation data
+    score = model.score(X_valid, y_valid)
 
-model.fit(X_train, y_train)
+    return score
 
+def main():
+    file_path = '../data/Course_Summary_2022_2023.json'
+    score = perform_linear_regression(file_path)
+    print("R-squared score: {:.2f}".format(score))
 
-score = model.score(X_valid, y_valid)
-
-print("R-squared score: {:.2f}".format(score))
+if __name__ == '__main__':
+    main()
