bedc6471093f32fb5c9ef14fc7ef6c71607c0b9d
jjcarrcannings <jjcarrcannings@gmail.com>
2023-06-12 13:59:36 -0700

    added new endpoint urls

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,7 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('predict/', views.my_endpoint, name='predict'),
+    path('predict/', views.predict, name='predict'),
+    path('predict-detailed/', views.predict_detailed, name='predict-detailed'),
+    path('train/', views.train, name='train'),
+    path('train-detailed/', views.train_detailed, name='train-detailed'),
 ]
 
 
