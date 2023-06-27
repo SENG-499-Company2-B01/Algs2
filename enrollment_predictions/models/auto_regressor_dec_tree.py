c013e3ac3e19f56d5379fd40c7318a28fb41a656
samuel-barrett <90578414+samuel-barrett@users.noreply.github.com>
2023-06-26 17:56:39 -0700

    Fixed potential data leakage issue with decision tree

--- enrollment_predictions/models/auto_regressor_dec_tree.py
+++ enrollment_predictions/models/auto_regressor_dec_tree.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from sklearn.metrics import mean_absolute_error, mean_squared_error
 from lightgbm import LGBMRegressor
 import matplotlib.patches as mpatches
+import re
 
 def load_enrollment_data(file_path):
     try:
@@ -14,51 +15,76 @@ def load_enrollment_data(file_path):
         return None
 
 def data_preprocessing(data):
+    # Filter data
     data = data[data['Subj'].isin(['SENG', 'CSC', 'ECE'])]
     data = data[data['Sched Type'].isin(['LEC'])]
+
+    # Create new features
     data['Course'] = data['Subj'] + data["Num"]
     data['Year'] = data['Term'].astype(int) // 100
     term_month = data['Term'] % 100
     season_mapping = {5: 1, 9: 2, 1: 3}
     data['Season'] = term_month.map(season_mapping)
-    
+
     # Create unique identifier for each course offering
-    data['CourseOffering'] = data['Course'] + "-" + data['Year'].astype(str) + "-" + data['Season'].astype(str)
+    data['CourseOffering'] = data['Course'] + data['Year'].astype(str) + "-" + data['Season'].astype(str)
+
+    # One-hot encode the categorical features
+    data = pd.get_dummies(data, columns=['Instructor', 'Subj', 'Status', 'Section', 'Dept Desc', 'Faculty', 'Camp'])
+
+    # Remove special characters from feature names
+    data.columns = data.columns.map(lambda x: re.sub(r'[^a-zA-Z0-9_]', '_', x))
 
     return data
 
-def feature_engineering(data):
-    try:
-        window_sizes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-        for win in window_sizes:
-            data['mean_prev_{}'.format(win)] = data['Enrolled'].rolling(window=win).mean()
-            data['median_prev_{}'.format(win)] = data['Enrolled'].rolling(window=win).median()
-            data['min_prev_{}'.format(win)] = data['Enrolled'].rolling(window=win).min()
-            data['max_prev_{}'.format(win)] = data['Enrolled'].rolling(window=win).max()
-            data['std_prev_{}'.format(win)] = data['Enrolled'].rolling(window=win).std()
-            data['ewm_{}'.format(win)] = data['Enrolled'].ewm(span=win).mean()
+def remove_unnecessary_columns(data):
+    # Drop unnecessary columns
+    data.drop(columns=['Term', 'CRN', 'Num', 'Title', 'Units', 'Begin', 'End', 'Days', 'Start_Date', 'End_Date', 'Bldg', 'Room', 'Sched_Type', 'Course'], inplace=True)
+    return data
 
-        return data
-    except Exception as e:
-        print("Error occurred during feature engineering:", str(e))
-        return None
+def feature_engineering(data):
+    window_sizes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    course_groups = data.groupby('Course')
+
+    for win in window_sizes:
+        data['mean_prev_{}'.format(win)] = course_groups['Enrolled'].transform(lambda x: x.shift().rolling(window=win).mean())
+        data['median_prev_{}'.format(win)] = course_groups['Enrolled'].transform(lambda x: x.shift().rolling(window=win).median())
+        data['min_prev_{}'.format(win)] = course_groups['Enrolled'].transform(lambda x: x.shift().rolling(window=win).min())
+        data['max_prev_{}'.format(win)] = course_groups['Enrolled'].transform(lambda x: x.shift().rolling(window=win).max())
+        data['std_prev_{}'.format(win)] = course_groups['Enrolled'].transform(lambda x: x.shift().rolling(window=win).std())
+        data['ewm_{}'.format(win)] = course_groups['Enrolled'].transform(lambda x: x.shift().ewm(span=win).mean())
+
+    return data
+
 
 
 def prepare_data(data, first_year, train_end_year, predict_year):
-    train_data = data[(data['Year'] >= first_year) & (data['Year'] <= train_end_year)]
-    val_data = data[data['Year'] == predict_year]
+    # Perform train-test split
+    train_data = data[(data['Year'] >= first_year) & (data['Year'] <= train_end_year)].copy()
+    val_data = data[data['Year'] == predict_year].copy()
 
     if val_data.empty:
         print(f"Warning: No validation data for year {predict_year}.")
         return None, None, None, None
 
-    train_features = train_data.columns[train_data.columns.str.contains('prev_|ewm_')]
+    # Apply feature engineering to the train and validation sets separately
+    train_data = feature_engineering(train_data)
+    val_data = feature_engineering(val_data)
+    
+    train_data = remove_unnecessary_columns(train_data)
+    val_data = remove_unnecessary_columns(val_data)
+    
+
+    exclude_columns = ['Year', 'Season', 'Enrolled', 'CourseOffering'] 
+
+    train_features = train_data.columns[~train_data.columns.isin(exclude_columns)]
     train_target = ['Enrolled', 'CourseOffering'] 
 
-    val_features = val_data.columns[val_data.columns.str.contains('prev_|ewm_')]
+    val_features = val_data.columns[~val_data.columns.isin(exclude_columns)]
     val_target = ['Enrolled', 'CourseOffering'] 
 
     return train_data[train_features], train_data[train_target], val_data[val_features], val_data[val_target]
+
 
 def model_training(X_train, y_train):
     try:
@@ -130,10 +156,10 @@ def main():
     if data is None or data.empty:
         print("Error: Failed during data preprocessing.")
         return
-    data = feature_engineering(data)
-    if data is None or data.empty:
-        print("Error: Failed during feature engineering.")
-        return
+    #data = feature_engineering(data)
+    #if data is None or data.empty:
+    #    print("Error: Failed during feature engineering.")
+    #    return
 
     first_year = data['Year'].min()
     last_year = data['Year'].max()
