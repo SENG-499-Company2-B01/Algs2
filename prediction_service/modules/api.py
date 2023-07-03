f1fa48fe55e73bb5d32db7b0b12baad8901ed56f
Erik McKelvey <erik.mckelvey.is@gmail.com>
2023-07-03 14:33:29 -0700

    Added BACKEND_TOKEN env and apikey header

--- prediction_service/modules/api.py
+++ prediction_service/modules/api.py
@@ -2,13 +2,16 @@ import requests
 from os import environ
 
 def request_courses():
-    courses = requests.get(f'{environ["BACKEND_URL"]}/courses')
+    headers = {'apikey': environ['BACKEND_TOKEN']}
+    courses = requests.get(f'{environ["BACKEND_URL"]}/courses', headers=headers)
     return courses
 
 def request_schedules():
-    schedules = requests.get(f'{environ["BACKEND_URL"]}/schedules')
+    headers = {'apikey': environ['BACKEND_TOKEN']}
+    schedules = requests.get(f'{environ["BACKEND_URL"]}/schedules', headers=headers)
     return schedules
 
 def request_historic_schedules():
-    schedules = requests.get(f'{environ["BACKEND_URL"]}/getSchedule/prev')
+    headers = {'apikey': environ['BACKEND_TOKEN']}
+    schedules = requests.get(f'{environ["BACKEND_URL"]}/getSchedule/prev', headers=headers)
     return schedules
