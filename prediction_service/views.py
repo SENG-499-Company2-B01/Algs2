b342657388cf974aa94cd98016e7c1cb0bfc083f
mattmc-00 <matthewmclean@uvic.ca>
2023-07-06 09:39:23 -0700

    Added predictor that simply takes most recent enrollment

--- prediction_service/views.py
+++ prediction_service/views.py
@@ -3,6 +3,7 @@ from .modules import utils
 from django.http import HttpResponse, JsonResponse
 from enrollment_predictions.enrollment_predictions import enrollment_predictions
 import json
+import pandas as pd
 
 def predict(request):
     # Check that request is a POST request
@@ -36,6 +37,8 @@ def predict(request):
     ## Get courses from backend
     ## courses = api.request_courses()
 
+    predictions = most_recent_enrollments(historic_schedules, courses)
+    """ TODO: Uncomment when decision tree is ready
     courses = utils.filter_courses_by_term(courses, term)
 
     # Reformat courses for prediction
@@ -45,7 +48,7 @@ def predict(request):
     predictions = enrollment_predictions(historic_schedules, courses)
 
     # Reformate predictions
-    predictions = utils.reformat_predictions(courses, predictions)
+    predictions = utils.reformat_predictions(courses, predictions)"""
     
     return JsonResponse(predictions, status=200) 
 
@@ -64,3 +67,59 @@ def predict(request):
         # score = perform_decision_tree()
     #TODO: Return predictions to backend (json)
     '''
+
+def most_recent_enrollments(historic_schedules, courses):
+    historic_schedules = utils.reformat_schedules(historic_schedules)
+    historic_schedules = pd.DataFrame(historic_schedules)
+    historic_schedules['Course'] = historic_schedules['Subj'] + historic_schedules["Num"]
+    term_month = historic_schedules['Term'].astype(int) % 100
+    season_mapping = {5: 1, 9: 2, 1: 3}
+    historic_schedules['Season'] = term_month.map(season_mapping)
+
+    courses = utils.filter_courses_by_term(courses, term)
+    courses = utils.reformat_courses(courses, year, term)
+    courses = pd.DataFrame(courses)
+    courses['Course'] = courses['Subj'] + courses["Num"]
+    term_month = courses['Term'].astype(int) % 100
+    season_mapping = {5: 1, 9: 2, 1: 3}
+    courses['Season'] = term_month.map(season_mapping)
+
+    predictions = []
+    for ind in courses.index:
+        past_offerings = historic_schedules[historic_schedules["Course"] == courses['Course'][ind]]
+        past_offerings = past_offerings[past_offerings["Season"] == courses['Season'][ind]]
+        most_recent_offering = past_offerings[past_offerings["Term"] == past_offerings["Term"].max()].iloc[0]
+
+        prediction = {
+            "course" : courses['Course'][ind],
+            "estimate" : most_recent_offering["Enrolled"]
+        }
+        predictions.append(prediction)
+
+    return(predictions)
+
+if __name__ == '__main__':
+    with open('data/client_data/schedules.json', 'r', encoding='utf-8') as fh:
+        historic_schedules = json.load(fh)
+
+    courses = [
+        {
+            "name": "Fundamentals of Programming with Engineering Applications",
+            "shorthand": "CSC115",
+            "prerequisites": [["CSC110"], ["CSC111"]],
+            "corequisites": [[""]],
+            "terms_offered": ["fall", "spring", "summer"]
+        },
+        {
+            "name": "Foundations of Computer Science",
+            "shorthand": "CSC320",
+            "prerequisites": [["CSC225"], ["CSC226"]],
+            "corequisites": [[""]],
+            "terms_offered": ["fall", "spring", "summer"]
+        }
+    ]
+    term = 'fall'
+    year = '2023'
+    
+    predictions = most_recent_enrollments(historic_schedules, courses)
+    print(predictions)
