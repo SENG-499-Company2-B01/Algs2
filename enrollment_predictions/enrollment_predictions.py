e890901c710b2522880cba0d3336d21f2f5df848
mattmc-00 <matthewmclean@uvic.ca>
2023-07-06 11:36:13 -0700

    Cleaning up views

--- enrollment_predictions/enrollment_predictions.py
+++ enrollment_predictions/enrollment_predictions.py
@@ -9,7 +9,7 @@ def enrollment_predictions(train_data, X):
     return predict_year(model, X) # Returns a dataframe with one column: 'Predicted'
 
 def most_recent_enrollments(historic_schedules, courses):
-    historic_schedules = data_preprocessing(historic_schedules)
-    result = predict_year(historic_schedules, courses)
+    historic_schedules = most_recent_data_preprocessing(historic_schedules)
+    result = most_recent_predict_year(historic_schedules, courses)
 
     return(result)
