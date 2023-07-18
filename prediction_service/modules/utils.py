2460bd4d2277bf22e0cd7b39b6f7c81a4f36c7a4
mattmc-00 <matthewmclean@uvic.ca>
2023-07-18 10:26:41 -0700

    Formatting for enrolled vs num_registered, temp

--- prediction_service/modules/utils.py
+++ prediction_service/modules/utils.py
@@ -99,7 +99,8 @@ def reformat_schedules(schedules):
                 subj, num = _shorthand_to_subj_and_num(course["course"])
                 enrolled = 0
                 for section in course["sections"]:
-                    enrolled += section["num_registered"]
+                    # enrolled += section["num_registered"]
+                    enrolled += section["enrolled"]
                 
                 courses.append({
                     "Term": str(schedule["year"]) + _term_plain_to_code(term["term"]),
