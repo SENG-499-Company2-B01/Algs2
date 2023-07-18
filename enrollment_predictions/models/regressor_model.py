Fixed duplication errors (and data leak)
Also added two new files which split up functions into testing and actual ml functions

enrollment_predictions/models/regressor_model.py
@@ -1,64 +1,10 @@
 import pandas as pd
 import numpy as np
+from sklearn.impute import SimpleImputer
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import r2_score
-from sklearn.impute import SimpleImputer
-from sklearn.model_selection import GridSearchCV
-from sklearn.ensemble import RandomForestRegressor
-if __name__ == "__main__":
-    import matplotlib.patches as mpatches
-    import matplotlib.pyplot as plt
-
-
-def flatten_data(data):
-    """ This function flattens the nested JSON data into a flat dataframe
-
-    Args:
-        data (pd.DataFrame): The JSON data
-    """
-    rows = []
-    for _, row in data.iterrows():
-        year = row["year"]
-        terms = row["terms"]
-
-
-        for term_data in terms:
-            term = term_data["term"]
-
-
-            course_enrollments = {}
-            for course_data in term_data["courses"]:
-                course = course_data["course"]
-                sections = course_data["sections"]
-
-
-                total_enrollment = 0
-                for section_data in sections:
-                    if section_data["num"].startswith('A'):
-                        total_enrollment += section_data["enrolled"]
-
-                course_enrollments[course] = total_enrollment
-
-
-            for course, enrollment in course_enrollments.items():
-                rows.append({
-                    "year": year,
-                    "term": term,
-                    "course": course,
-                    "enrolled": enrollment
-                })
-
-
-    return pd.DataFrame(rows)
-
-
-def load_enrollment_data(file_path):
-    try:
-        return pd.read_json(file_path)
-    except FileNotFoundError:
-        print(f"Error: File '{file_path}' not found.")
-        return None
 
 
 def data_preprocessing(data):
@@ -73,7 +19,7 @@ def data_preprocessing(data):
         'fall': 2,
         'spring': 3
     }
-    
+
     data['term'] = data['term'].map(season_mapping)
     # Create unique identifier for each course offering
     data['CourseOffering'] = data['course'] + \
@@ -82,7 +28,7 @@ def data_preprocessing(data):
     # Map course to numerical values:
     # SENGXXX -> 1XXX, CSCXXX -> 2XXX, ECE -> 3XXX
     subj_mapping = {'SENG': 1000, 'CSC': 2000, 'ECE': 3000}
-    
+
     # Course number is the last 3 digits of the course
     data['course'] = data['course'].str.extract(r'(\d+)').astype(int)
     data['course'] = data['course'] + data['subj'].map(subj_mapping)
@@ -130,25 +76,6 @@ def prepare_data(data, first_year, train_end_year, predict_year):
     return X_train_imputed, train_target, X_val_imputed, val_target
 
 
-def model_training_grid_search(X_train, y_train, model):
-    param_grid = {
-        'n_estimators': [11, 13, 15, 18, 20, 25],
-        'max_depth': [80, 85, 90, 95, 100],
-        'min_samples_split': [1, 2, 3, 4, 5],
-        'min_samples_leaf': [1, 2],
-    }
-    try:
-        grid_search = GridSearchCV(estimator=model, param_grid=param_grid, cv=5, scoring='neg_mean_absolute_error', verbose=2, n_jobs=-1)
-        grid_search.fit(X_train, y_train['enrolled'])
-        best_model = grid_search.best_estimator_
-        print("Best Parameters: ", grid_search.best_params_)
-
-        return best_model
-    except Exception as e:
-        print("Error occurred during model training:", str(e))
-        return best_model
-
-
 def model_training(X_train, y_train, model):
     try:
         model.fit(X_train, y_train['enrolled'])
@@ -168,28 +95,6 @@ def model_evaluation(model, X_val, y_val):
     rmse = np.sqrt(mean_squared_error(y_val['enrolled'], y_pred))
     r2 = r2_score(y_val['enrolled'], y_pred)
     return predictions_df, mae, rmse, r2
-
-
-def plot_results(pred_df, y_val, predict_year):
-    plt.figure(figsize=(18, 10))
-    courses = pred_df['CourseOffering'].unique()
-    for i, course in enumerate(courses):
-        course_df = pred_df[pred_df['CourseOffering'] == course]
-        actual_val = y_val[y_val['CourseOffering'] == course]['enrolled']
-
-        plt.scatter([course]*len(course_df), actual_val, color='b')
-        plt.scatter([course]*len(course_df), course_df['Predicted'], color='r')
-
-    plt.xlabel('Course Offering')
-    plt.xticks(rotation=90)
-    plt.ylabel('Enrollment Prediction')
-    plt.title(f'Enrollment Prediction for Year {predict_year}')
-
-    actual_patch = mpatches.Patch(color='b', label='Actual')
-    predicted_patch = mpatches.Patch(color='r', label='Predicted')
-    plt.legend(handles=[actual_patch, predicted_patch])
-
-    plt.show()
 
 
 def run_prediction_for_year(data, first_year, train_end_year):
@@ -212,32 +117,3 @@ def run_prediction_for_year(data, first_year, train_end_year):
     print(f'For prediction year {predict_year}: MAE = {mae}, RMSE = {rmse}, R^2 = {r2}')
 
     return pred_df, y_val, predict_year
-
-
-def main():
-    enrollment_data_path = "./data/client_data/schedules.json"
-    data = load_enrollment_data(enrollment_data_path)
-    if data is None or data.empty:
-        print("Error: Empty data or failed to load data.")
-        return
-    data = flatten_data(data)
-    if data is None or data.empty:
-        print("Error flattening data")
-        return
-    data = data_preprocessing(data)
-    if data is None or data.empty:
-        print("Error: Failed during data preprocessing.")
-        return
-
-    first_year = data['year'].min()
-    last_year = data['year'].max()
-    for train_end_year in range(last_year-1, last_year):
-        pred_df, y_val, predict_year = run_prediction_for_year(
-            data, first_year, train_end_year)
-
-        if pred_df is not None:
-            plot_results(pred_df, y_val, predict_year)
-
-
-if __name__ == "__main__":
-    main()